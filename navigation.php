c89b70c060049ef0ab5680f96af50a26da7fa925
princeDrei <beedzreynidolagahit@gmail.com>
2024-09-12 17:15:26 +0800

    Added navigation.php file for branch2

---
+++ navigation.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus doloribus consequatur animi, delectus optio dolores vitae minus rem quos suscipit iusto. Eveniet earum soluta sapiente est dignissimos non inventore accusantium.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum dolore vero, veniam maiores voluptatum itaque, nesciunt repudiandae nemo porro mollitia unde rem assumenda error quae deserunt dolorem saepe neque iure.</p>
+</body>
+</html>
